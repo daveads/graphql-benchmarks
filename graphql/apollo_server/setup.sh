d91ef49fc1bd2057ad32a4884f9e11135d2adc52
daveads <the.daveads@gmail.com>
2024-07-19 15:11:34 +0100

    ..

--- graphql/apollo_server/setup.sh
+++ graphql/apollo_server/setup.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# Start the timer
-start_time=$(date +%s.%N)
-
 # For apollo server:
 cd graphql/apollo_server
 npm i
 cd ../../
 
-# End the timer
-end_time=$(date +%s.%N)
 
-# Calculate the duration
-duration=$(echo "$end_time - $start_time" | bc)
-
-# Convert to minutes and seconds
-minutes=$(echo "$duration / 60" | bc)
-seconds=$(echo "$duration % 60" | bc)
-
-# Print the execution time
-printf "Apollo Setup.sh >>> Execution time: %d minutes and %.2f seconds\n" $minutes $seconds
