ef0e7c9e4ff308c2d77b3abc2d1201634e2dd3ca
daveads <the.daveads@gmail.com>
2024-07-21 14:49:44 +0100

    .

--- graphql/apollo_server/run.sh
+++ graphql/apollo_server/run.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-# Start the timer
-start_time=$(date +%s)
-
 pwd
 cd graphql/apollo_server
 npm i
 npm start
-
-# End the timer
-end_time=$(date +%s)
-
-# Calculate the duration
-duration=$((end_time - start_time))
-
-# Convert to hours, minutes, and seconds
-hours=$((duration / 3600))
-minutes=$(( (duration % 3600) / 60 ))
-seconds=$((duration % 60))
-
-# Print the execution time
-printf " apollo_server Run.sh >>> Execution time: %d hours, %d minutes, and %d seconds\n" $hours $minutes $seconds
--- graphql/tailcall/setup.sh
+++ graphql/tailcall/setup.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# Start timing
-start_time=$(date +%s)
-
 # For tailcall:
 cd graphql/tailcall
 npm install
 cd ../../
-
-# End timing
-end_time=$(date +%s)
-
-# Calculate duration
-duration=$((end_time - start_time))
-
-# Convert seconds to minutes and seconds
-minutes=$((duration / 60))
-seconds=$((duration % 60))
-
-echo "tailcall SETUP.sh >>> Script execution time: $minutes minutes and $seconds seconds"
