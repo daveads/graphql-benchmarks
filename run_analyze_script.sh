4c7ef62a1dabda6aa7748bd68514f2b636d99ed6
daveads <the.daveads@gmail.com>
2024-07-22 15:06:30 +0100

    using bash

--- run_analyze_script.sh
+++ run_analyze_script.sh
@@ -7,7 +7,7 @@ rm results.md
 for bench in 1 2 3; do
     if ls bench${bench}*.txt &> /dev/null; then
         echo "Processing files for bench${bench}:"
-        node analyze.js bench${bench}*.txt
+        bash analyze.sh bench${bench}*.txt
         echo "Files processed: $(ls bench${bench}*.txt)"
     else
         echo "No matching files found for bench${bench}*.txt"
