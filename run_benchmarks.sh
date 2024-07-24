8cb638a0cba9ccb809443d8dd44c7b25b6cbc58d
daveads <the.daveads@gmail.com>
2024-07-24 01:45:16 +0100

    see run_benchmarks.sh in parallel

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -38,12 +38,13 @@ function runBenchmark() {
     graphqlEndpoint=http://127.0.0.1:8080/v1/graphql
   fi
 
-  for bench in "${benchmarks[@]}"; do
+  # Replace / with _
+  local sanitizedServiceScriptName=$(echo "$serviceScript" | tr '/' '_')
+
+  # Function to run a single benchmark
+  function runSingleBenchmark() {
+    local bench="$1"
     local benchmarkScript="wrk/bench.sh"
-
-    # Replace / with _
-    local sanitizedServiceScriptName=$(echo "$serviceScript" | tr '/' '_')
-
     local resultFiles=("result1_${sanitizedServiceScriptName}.txt" "result2_${sanitizedServiceScriptName}.txt" "result3_${sanitizedServiceScriptName}.txt")
 
     bash "test_query${bench}.sh" "$graphqlEndpoint"
@@ -56,19 +57,27 @@ function runBenchmark() {
     bash "$benchmarkScript" "$graphqlEndpoint" "$bench" >/dev/null
     sleep 1
 
-        # 3 benchmark runs
-        for resultFile in "${resultFiles[@]}"; do
-            echo "Running benchmark $bench for $serviceScript"
-            bash "$benchmarkScript" "$graphqlEndpoint" "$bench" >"bench${bench}_${resultFile}"
-            if [ "$bench" == "1" ]; then
-                bench1Results+=("bench1_${resultFile}")
-            elif [ "$bench" == "2" ]; then
-                bench2Results+=("bench2_${resultFile}")
-            elif [ "$bench" == "3" ]; then
-                bench3Results+=("bench3_${resultFile}")
-            fi
-        done
+    # 3 benchmark runs
+    for resultFile in "${resultFiles[@]}"; do
+        echo "Running benchmark $bench for $serviceScript"
+        bash "$benchmarkScript" "$graphqlEndpoint" "$bench" >"bench${bench}_${resultFile}"
+        if [ "$bench" == "1" ]; then
+            bench1Results+=("bench1_${resultFile}")
+        elif [ "$bench" == "2" ]; then
+            bench2Results+=("bench2_${resultFile}")
+        elif [ "$bench" == "3" ]; then
+            bench3Results+=("bench3_${resultFile}")
+        fi
     done
+  }
+
+  # Run benchmarks in parallel
+  runSingleBenchmark 1 &
+  runSingleBenchmark 2 &
+  runSingleBenchmark 3 &
+
+  # Wait for all benchmarks to complete
+  wait
 }
 
 rm "results.md"
@@ -88,28 +97,25 @@ if [[ ! " ${valid_services[@]} " =~ " ${service} " ]]; then
     exit 1
 fi
 
-
-
 runBenchmark "graphql/${service}/run.sh"
-
 
 echo "Benchmark 1"
 cat ./bench1_result1_graphql_${service}_run.sh.txt
 cat ./bench1_result2_graphql_${service}_run.sh.txt
 cat ./bench1_result3_graphql_${service}_run.sh.txt
-echo "End of Becnhmark 1"
+echo "End of Benchmark 1"
 echo ""
 echo "Benchmark 2"
 cat ./bench2_result1_graphql_${service}_run.sh.txt
 cat ./bench2_result2_graphql_${service}_run.sh.txt
 cat ./bench2_result3_graphql_${service}_run.sh.txt
-echo "End of Becnhmark 2"
+echo "End of Benchmark 2"
 echo ""
-echo "Becnhmark 3"
+echo "Benchmark 3"
 cat ./bench3_result1_graphql_${service}_run.sh.txt
 cat ./bench3_result2_graphql_${service}_run.sh.txt
 cat ./bench3_result3_graphql_${service}_run.sh.txt
-echo "End of Benchmark 2"
+echo "End of Benchmark 3"
 echo ""
 
 if [ "$service" == "apollo_server" ]; then
